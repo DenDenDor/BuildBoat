82dbdd816a72175051b9a3f320726ca0169680e0
GameFit <gamecrafting77@gmail.com>
2025-05-01 17:50:58 +0300

    Add chest system

--- BuildBoat/Assets/Scripts/UI/LootGenerator.cs
+++ BuildBoat/Assets/Scripts/UI/LootGenerator.cs
@@ -1,6 +1,10 @@
+using System;
 using DG.Tweening;
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Random = UnityEngine.Random;
 
 public enum LootType
 {
@@ -14,27 +18,85 @@ public enum LootType
 public class LootGenerator : MonoBehaviour
 {
     [SerializeField] private ItemViewer _itemViewer;
-    [SerializeField] private Sprite _testItemSprite;
+    [SerializeField] private List<ChestData> _chestDatas;
+
+    [Header("Block Icons")]
+    [SerializeField] private List<BlockIconPair> _blockIconList = new List<BlockIconPair>();
+
+    private Dictionary<BlockType, Sprite> _blockIconsDictionary = new Dictionary<BlockType, Sprite>();
+
+    private LootPanel _lootPanel;
     
-    public void GetLoot(LootType lootType, Color rareColor)
+    private void Awake()
     {
-        _itemViewer.gameObject.SetActive(true);
-        _itemViewer.View(_testItemSprite,2, rareColor);
-        int itemsCount = 4;
-        StartCoroutine(ItemsViewing(itemsCount));
+        _blockIconsDictionary = _blockIconList.ToDictionary(pair => pair.GetBlockType, pair => pair.GetIcon);
     }
 
-    private IEnumerator ItemsViewing(int itemsCount)
+    private void Start()
     {
-        for (int i = 0; i < itemsCount; i++)
+        _lootPanel = GetComponent<LootPanel>();
+    }
+    
+    public void GetLoot(LootType lootType)
+    {
+        ChestData chestData = _chestDatas.FirstOrDefault(data => data.GetLootType == lootType);
+
+        if (chestData == null)
+        {
+            Debug.LogError("No ChestData found for LootType: " + lootType);
+            return;
+        }
+
+        List<KeyValuePair<BlockType, int>> droppedItems = GenerateLoot(chestData);
+
+        _itemViewer.gameObject.SetActive(true);
+
+        StartCoroutine(ShowLoot(droppedItems, chestData.RareColor));
+    }
+    
+    private List<KeyValuePair<BlockType, int>> GenerateLoot(ChestData chestData)
+    {
+        List<KeyValuePair<BlockType, int>> droppedItems = new List<KeyValuePair<BlockType, int>>();
+
+        foreach (LootItem lootItem in chestData.GuaranteedLoot)
+        {
+            int amount = Random.Range(lootItem.MinAmount, lootItem.MaxAmount + 1);
+            droppedItems.Add(new KeyValuePair<BlockType, int>(lootItem.GetBlockType, amount));
+        }
+
+        int possibleItemsCount = 0;
+        foreach (LootItem lootItem in chestData.PossibleLoot)
+        {
+            if (possibleItemsCount < chestData.MaxPossibleItems && Random.value <= lootItem.DropChance)
+            {
+                int amount = Random.Range(lootItem.MinAmount, lootItem.MaxAmount + 1);
+                droppedItems.Add(new KeyValuePair<BlockType, int>(lootItem.GetBlockType, amount));
+                possibleItemsCount++;
+            }
+        }
+
+        return droppedItems;
+    }
+
+    
+    private IEnumerator ShowLoot(List<KeyValuePair<BlockType, int>> droppedItems, Color rareColor)
+    {
+        foreach (var item in droppedItems)
         {
             DOTween.Sequence()
-                .Append(_itemViewer.transform.DOScale(1.2f, 0.2f))
-                .Append(_itemViewer.transform.DOScale(1f, 0.05f));
-            
-            _itemViewer.View(_testItemSprite,2, Color.blue);
-            
-            yield return new WaitForSeconds(1f);
+                .Append(_itemViewer.transform.DOScale(1.3f, 0.3f))
+                .Append(_itemViewer.transform.DOScale(1f, 0.12f))
+                .AppendInterval(0.25f)
+                .Append(_itemViewer.transform.DOScale(0f, 0.2f));
+
+            Sprite icon = GetIconForBlockType(item.Key);
+
+            _itemViewer.View(icon, item.Value, rareColor);
+            yield return new WaitForSeconds(0.7f);
         }
+
+        _lootPanel.ClosePanel();
     }
+    
+    private Sprite GetIconForBlockType(BlockType blockType) => _blockIconsDictionary[blockType];
 }
--- BuildBoat/Assets/Scripts/UI/LootPanel.cs
+++ BuildBoat/Assets/Scripts/UI/LootPanel.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 public class LootPanel : MonoBehaviour
 {
     public static LootPanel Instance {get; private set;}
+    
     [SerializeField] private Button _chest;
     [SerializeField] private Image _backLight;
 
@@ -15,13 +16,10 @@ public class LootPanel : MonoBehaviour
     [SerializeField] private RectTransform _fingerEndPos;
 
     [SerializeField] private Color _offChestColor;
-    [SerializeField] private Sprite _testOpenChest;
     
     private LootGenerator _lootGenerator;
     
     private Tween _fingerMoveTween;
-
-    private Color _chestRareColor;
 
     private LootType _chestType;
 
@@ -35,28 +33,42 @@ public class LootPanel : MonoBehaviour
     
     private void Start()
     {
+        _fingerObject.GetComponent<RectTransform>().DOAnchorPos(_fingerEndPos.anchoredPosition, 0.25f).SetLoops(-1, LoopType.Yoyo);
         _lootGenerator = GetComponent<LootGenerator>();
     }
 
     public void LootPanelOpen(Sprite chestSprite, Color backLightColor, LootType chestRare)
     {
-        _fingerObject.GetComponent<RectTransform>().DOAnchorPos(_fingerEndPos.anchoredPosition, 0.25f).SetLoops(-1, LoopType.Yoyo);
-        
         _fingerObject.SetActive(true);
-
-        _chestRareColor = backLightColor;
 
         _itemsBackLight.gameObject.SetActive(false);
 
         _lootPanel.SetActive(true);
-
+        
+        _chest.gameObject.SetActive(true);
+        _chest.transform.localScale = Vector3.one;
         _chest.image.color = Color.white;
         _chest.image.sprite = chestSprite;
+        
         _backLight.color = backLightColor;
+        
+        _chestType = chestRare;
         
         _chest.onClick.AddListener(ChestOpen);
     }
 
+    public void ClosePanel()
+    {
+        _itemsBackLight.DOScale(0, 0.09f);
+        DOTween.Sequence()
+            .Append(_chest.transform.DOScale(0, 0.2f))
+            .Append(_backLight.DOColor(Color.clear, 0.2f))
+            .OnComplete(() =>
+            {
+                _lootPanel.SetActive(false);
+            });
+    }
+    
     private void ChestOpen()
     {
         _chest.onClick.RemoveListener(ChestOpen);
@@ -79,6 +91,6 @@ public class LootPanel : MonoBehaviour
         _itemsBackLight.gameObject.SetActive(true);
         _itemsBackLight.DOScale(1f, 0.1f);
 
-        _lootGenerator.GetLoot(_chestType, _chestRareColor);
+        _lootGenerator.GetLoot(_chestType);
     }
 }
